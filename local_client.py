0a8b993cee5d2734fc40178f6ed6798ba9b82f2a
Omri Abo <omriabo.gmail.com>
2023-01-26 11:35:06 +0200

    dummy client update

--- local_client.py
+++ local_client.py
@@ -1,3 +1,3 @@
 import requests
 
-print(requests.get('http://127.0.0.1:5000/similar_users?', params={'user_id': 1, 'user_num': 1}))
+print(requests.get('http://3.101.13.147:8080/similar_users?', params={'user_id': 1, 'user_num': 1}).text)

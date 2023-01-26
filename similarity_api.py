60d999624a300b6aca57a76c9d85b0698834357f
Omri Abo <omriabo.gmail.com>
2023-01-26 13:01:45 +0200

    api modified

--- similarity_api.py
+++ similarity_api.py
@@ -5,8 +5,8 @@ from flask import request
 
 app = Flask(__name__)
 
-db_df = pd.read_csv('file.csv')
-numeric_df = pd.read_csv('preprocessed_data.csv.csv')
+db_df = pd.read_csv('profiles_for_db.csv')
+numeric_df = pd.read_csv('preprocessed_data.csv')
 
 
 #app will filter according to same language and location and different ethnicity (and id)
@@ -14,19 +14,18 @@ numeric_df = pd.read_csv('preprocessed_data.csv.csv')
 #it will sort the list by distance (ascending), map the ids accordingly and send back the ids ordered
 @app.route('/similar_users')
 def get_closest_users():
-    user_id = request.args.get('user_id')
-    user_num = request.args.get('user_num')
-    user_data = db_df[db_df['user_id'] == user_id]
-    user_array = numeric_df[numeric_df['user_id'] == user_id]
-    suited_users = db_df[db_df['location'] == user_data['location']
-                         & db_df['language'] == user_data['language']
-                         & db_df['ethnicity'] != user_data['ethnicity']
-                         & db_df['user_id'] != user_data['user_id']]
+    user_id = int(request.args.get('user_id'))
+    user_num = int(request.args.get('user_num'))
+    user_data = db_df.loc[db_df['user_id'] == user_id]
+    user_array = numeric_df.loc[numeric_df['user_id'] == user_id]
+    suited_users = db_df.loc[(db_df['ethnicity'] != str(user_data['ethnicity']))]
+    print(suited_users)
     user_array = user_array.drop(columns='user_id')
     suited_numeric = numeric_df[~(numeric_df.user_id.isin(suited_users.user_id))]
     distances = dict()
     for user in suited_numeric:
-        distance = cityblock(user_array, user.drop(columns='user_id'))
+        no_id = user.drop(columns='user_id')
+        distance = cityblock(user_array, no_id)
         distances[user['user_id']] = distance
     sorted_distances = sorted(distances.items(), key=lambda x: x[1])
     ids_by_distance = [i[0] for i in sorted_distances]
@@ -35,4 +34,5 @@ def get_closest_users():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080)
+    app.run()
